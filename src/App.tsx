066aa3378bec3e35274d9251669dcd29e0fd43dd
Taylan Pince <taylanpince@gmail.com>
2025-04-04 12:16:03 +0200

    Add generic OIDC implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import { useEffect, useState } from 'react'
-import { sequenceWaas, googleClientId } from "./SequenceEmbeddedWallet";
-
+import { sequenceWaas } from "./SequenceEmbeddedWallet";
 import './App.css'
-import {
-  CredentialResponse,
-  GoogleLogin,
-  GoogleOAuthProvider,
-} from "@react-oauth/google";
 
 function App() {
   const [walletAddress, setWalletAddress] = useState<any>(null)
+  const [jwtToken, setJwtToken] = useState('')
 
-  const handleGoogleLogin = async (tokenResponse: CredentialResponse) => {
+  const handleJwtLogin = async () => {
     try {
       const res = await sequenceWaas.signIn(
         {
-          idToken: tokenResponse.credential!,
+          idToken: jwtToken,
         },
-        'Google Embedded Wallet React Boilerplate'
+        'Generic OIDC Embedded Wallet React Boilerplate'
       );
 
       setWalletAddress(res.wallet);
@@ -38,31 +33,44 @@ function App() {
       console.log(err)
     }
   }
+
   useEffect(() => {
     
   }, [walletAddress])
 
   return (
     <>
-    <h1 className='title'>Embedded Wallet Google Auth</h1>
+    <h1 className='title'>Embedded Wallet OIDC Auth</h1>
     <div style={{position: 'fixed', top:'60px', right: '60px'}}>
     {walletAddress&&<p style={{cursor: 'pointer'}} onClick={() =>signOut()}>sign out</p>}
     </div>
     <div style={{
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         height: '20vh',
-        margin: 'auto'
+        margin: 'auto',
+        gap: '1rem'
     }}>
-      {!walletAddress&&<GoogleOAuthProvider clientId={googleClientId}>
-          <GoogleLogin
-            key="google"
-            onSuccess={handleGoogleLogin}
-            width={100}
+      {!walletAddress && (
+        <>
+          <textarea
+            value={jwtToken}
+            onChange={(e) => setJwtToken(e.target.value)}
+            placeholder="Enter your JWT token here"
+            style={{
+              width: '400px',
+              height: '100px',
+              padding: '8px',
+              borderRadius: '4px',
+              border: '1px solid #ccc'
+            }}
           />
-        </GoogleOAuthProvider>}
-        <p>{walletAddress}</p>
+          <button onClick={handleJwtLogin}>Sign In with JWT</button>
+        </>
+      )}
+      <p>{walletAddress}</p>
     </div>
     </>
   )
